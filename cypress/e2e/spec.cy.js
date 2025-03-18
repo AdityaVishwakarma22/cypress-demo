3f99f337d2cc7c25c68e103e541d9e44acdf0f0b
AdityaVishwakarma22 <22avkvishwakarma22@gmail.com>
2025-03-18 17:14:07 +0530

    changes

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,28 +1,29 @@
-describe("Intercept with Cypress", () => {
-  it("Test api with simple intercept", () => {
-    cy.visit("https://jsonplaceholder.typicode.com/");
+describe("My First Test", () => {
+  it("Gets, types and asserts", () => {
+    cy.visit("https://example.cypress.io");
 
-    cy.intercept({
-      path: "/posts",
-    }).as("posts");
+    cy.contains("type").click();
 
-    cy.get('table:nth-of-type(1) td a[href="/posts"]').click();
-    cy.wait("@posts").then((inter) => {
-      cy.log(JSON.stringify(inter));
-      console.log(JSON.stringify(inter));
-      expect(inter.response.body).to.have.length(100);
-    });
+    // Should be on a new URL which
+    // includes '/commands/actions'
+    cy.url().should("include", "/commands/actions");
+
+    // Get an input, type into it
+    cy.get(".action-email").type("fake@email.com");
+
+    //  Verify that the value has been updated
+    cy.get(".action-email").should("have.value", "fake@email.com");
   });
 });
 
-// it.only("Test api with simple intercept", () => {
-//   cy.visit("https://jsonplaceholder.typicode.com/");
-//   cy.intercept("GET", "https://jsonplaceholder.typicode.com/posts", {
-//     statusCode: 200,
-//     body: [
-//       { id: 1, title: "Intercepted Post", body: "This is a mocked response" },
-//     ],
-//   }).as("posts");
-//   cy.get('table:nth-of-type(1) td a[href="/posts"]').click();
-//   cy.wait("@posts");
-// });
+it.only("Test api with simple intercept", () => {
+  cy.visit("https://jsonplaceholder.typicode.com/");
+  cy.intercept("GET", "/posts", {
+    statusCode: 200,
+    body: [
+      { id: 1, title: "Intercepted Post", body: "This is a mocked response" },
+    ],
+  }).as("posts");
+  cy.get('table:nth-of-type(1) td a[href="/posts"]').click();
+  cy.wait("@posts");
+});
